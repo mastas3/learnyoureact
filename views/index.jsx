18b6a90ad39ac3e79aa362908ea62614aa272863
mastas3 <rpsm90@gmail.com>
2016-12-05 20:10:10 +0200

    commit before 8 excerice

--- views/index.jsx
+++ views/index.jsx
@@ -4,7 +4,7 @@ export default class TodoBox extends React.Component{
   render() {
     return (<div className="todoBox">
     	<h1>Todos</h1>
-    	<TodoList />
+    	<TodoList data={this.props.data}/>
     	<TodoForm />
       </div>);
   }
@@ -12,12 +12,13 @@ export default class TodoBox extends React.Component{
 
 class TodoList extends React.Component {
 	render() {
+		var todo = this.props.data.map(function(obj) {
+			return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>
+		})
 		return (<div className="todoList">
 			<table style={{border: "2px solid black"}}>
 				<tbody>
-					<Todo title="Shopping">Milk</Todo>
-					<Todo title="Hair cut">13:00</Todo>
-					<Todo title="Learn React">15:00</Todo>
+					{todo}
 				</tbody>
 			</table>
 		</div>);
